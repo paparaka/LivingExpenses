d6861062b1920e22699755618d5903df8b42ebbf
Ivan Paparaka (Mac) <ivanpaparaka@gmail.com>
2016-02-17 21:24:02 +0900

    More expense types
    Added more expense types to the cvs file
    consolidated reading and plotting in a single function
    removed the bar plots - they are not useful

--- r-scripts/expenses-numbeo.R
+++ r-scripts/expenses-numbeo.R
@@ -1,19 +1,5 @@
-#source("r-scripts/init.R")
 
-expenses <- read.csv("import-tables/expenses.csv", header = TRUE, sep = ",")
-str(expenses)
-
-expenses.plot <- data.frame(vars = c("SUM","Rent","Utilities","Food","Transport"),
-                            names = c("Basic living expenses","Rent","Utilities","Food expenses","Commute expenses"),
-                            stringsAsFactors = FALSE
-)
-
-expenses[,expenses.plot$vars] <- 
-  expenses[,expenses.plot$vars] * currencies[as.character(expenses$Currency),"USD"]
-expenses$Currency <- "USD"
-
-###########################
-expenses.comon.plot <- function(p, this.legend = "bottom") {
+Expenses.Comon.Plot <- function(p, this.legend = "bottom") {
   
   #cols <- c("Japan" = "#d7191c","UK" = "#2c7bb6","USA" = "#7b3294", "Germany" = "#fdae61")
   
@@ -32,43 +18,66 @@ expenses.comon.plot <- function(p, this.legend = "bottom") {
   return (p)
 }
 
-expenses.bar.plot <- function(this.var, this.name) {
-  
-  expenses$City <- factor(expenses$City, levels = expenses$City[order(expenses[,this.var], decreasing = T)])
-  
-  p <- ggplot(expenses, aes_string(x = "City", y = this.var, fill = "Country")) + 
-    geom_bar(stat = "identity") +
-    labs(title = paste("Estimated monthly",this.name,"(USD)"))+
-    scale_y_continuous(expand = c(0,0))
-  
-  p <- expenses.comon.plot(p)
-  
-  return(p)
-}
+# Expenses.Bar.Plot <- function(this.var, this.name) {
+#   
+#   expenses$City <- factor(expenses$City, levels = expenses$City[order(expenses[,this.var], decreasing = T)])
+#   
+#   p <- ggplot(expenses, aes_string(x = "City", y = this.var, fill = "Country")) + 
+#     geom_bar(stat = "identity") +
+#     labs(title = paste("Estimated monthly",this.name,"(USD)"))+
+#     scale_y_continuous(expand = c(0,0))
+#   
+#   p <- Expenses.Comon.Plot(p)
+#   
+#   return(p)
+# }
 
-expenses.box.plot <- function(this.var, this.name) {
+Expenses.Box.Plot  <- function(expenses, this.var, this.name, this.currency) {
   
   p <- ggplot(expenses, aes_string(x = "Country", y = this.var, fill = "Country")) + 
     geom_boxplot()+
     #geom_jitter(width = 0.2) +
     geom_text_repel(aes(label = City), arrow = NULL) +
-    labs(title = paste("Estimated monthly",this.name,"(USD)"))
+    labs(title = paste("Estimated ",this.name,"(",this.currency,")"))
   
-  p <- expenses.comon.plot(p, "none")
+  p <- Expenses.Comon.Plot(p, "none")
   
   return(p)
 }
 
-
-for (i in 1:nrow(expenses.plot)) {
+Expenses.Load.n.PLot <- function(filename = "import-tables/expenses.csv", currency = "USD") {
   
-  p1 <- expenses.bar.plot(expenses.plot$vars[i],expenses.plot$names[i])
-  p2 <- expenses.box.plot(expenses.plot$vars[i],expenses.plot$names[i])
+  expenses <- read.csv(filename, header = TRUE, sep = ",")
+  # str(expenses)
   
-  ggsave(paste0("export-png/numbeo-bar-",expenses.plot$names[i],".png"),p1, width = 12, height = 9, dpi = 300)
-  ggsave(paste0("export-pdf/numbeo-bar-",expenses.plot$names[i],".pdf"),p1, width = 12, height = 9)
-  ggsave(paste0("export-png/numbeo-box-",expenses.plot$names[i],".png"),p2, width = 12, height = 9, dpi = 300)
-  ggsave(paste0("export-pdf/numbeo-box-",expenses.plot$names[i],".pdf"),p2, width = 12, height = 9)
+  expenses.plot <- data.frame(vars = c("SUM","Rent","Utilities","Food","Transport",
+                                       "Meal.Inexpensive", "Beer", "Cappucino"),
+                              names = c("monthly Basic living expenses","monthly Rent","monthly Utilities","monthly Food expenses","monthly Commute expenses", 
+                                        "Meal Inexpensive", "Beer Draft", "Cappucino"),
+                              stringsAsFactors = FALSE
+  )
+  
+  expenses[,expenses.plot$vars] <- 
+    expenses[,expenses.plot$vars] * currencies[as.character(expenses$Currency), currency]
+  expenses$Currency <- currency
+  
+  #TODO implement a try() catch() method to skip errors
+  
+  for (i in 1:nrow(expenses.plot)) {
+    
+    #p1 <- Expenses.Bar.Plot(expenses.plot$vars[i],expenses.plot$names[i])
+    p2 <- Expenses.Box.Plot (expenses, expenses.plot$vars[i],expenses.plot$names[i],currency)
+    
+    # ggsave(paste0("export-png/numbeo-bar-",expenses.plot$names[i],".png"),p1, width = 12, height = 9, dpi = 300)
+    # ggsave(paste0("export-pdf/numbeo-bar-",expenses.plot$names[i],".pdf"),p1, width = 12, height = 9)
+    ggsave(paste0("export-png/numbeo-box-",expenses.plot$names[i],".png"),p2, width = 12, height = 9, dpi = 300)
+    ggsave(paste0("export-pdf/numbeo-box-",expenses.plot$names[i],".pdf"),p2, width = 12, height = 9)
+  }
+  
+  return(expenses)
 }
+
+
+
 
 
